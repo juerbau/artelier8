bc3b3a3e68d4b789a075e83fde20e1f8cb203313
juerbau <juerbau@web.de>
2026-05-26 23:04:48 +0200

    newsletter fix

--- app/newsletter/message/layout.jsx
+++ app/newsletter/message/layout.jsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import FadeInSection from "@/ui/components/FadeInSection";
 import Logo from "@/ui/components/Logo";
 
-export default async function NewsletterMessageLayout({children}) {
+export default function NewsletterMessageLayout({children}) {
 
     return (
         <main className={clsx(
@@ -13,7 +13,7 @@ export default async function NewsletterMessageLayout({children}) {
             "min-h-screen flex justify-center"
         )}>
             <div className="w-full max-w-xl text-center">
-                <FadeInSection>
+                <FadeInSection as="section">
                     <div className="mb-10 flex justify-center">
                         <Logo
                             href="/"
@@ -23,7 +23,7 @@ export default async function NewsletterMessageLayout({children}) {
                     </div>
                 </FadeInSection>
 
-                <FadeInSection delay={0.3}>
+                <FadeInSection as="section" delay={0.3}>
                     {children}
                 </FadeInSection>
             </div>
